d83df928675e80e5c252b81334c298933471ad2c
Michael Teixeira <meixeira@gmail.com>
2018-10-06 20:59:47 -0700

    Add server folder

---
+++ server/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // console.log('io', io);
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/test_bloom_payload', function(req, res, next) {
+  io.emit('bloom_payload', {_signatory: '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1'});
+  res.status(200).send();
+});
+
+
+router.post('/bloom_payload', function(req, res, next) {
+  io.emit('bloom_payload', req.body);
+  res.status(200).send();
+});
+
+module.exports = router;
